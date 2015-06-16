133c7a92c7fa8a70cf8f546889ec00be7ddd1eb3
zhouyang209117 <zhouyang209117@sina.com>
2015-06-16 08:22:51 +0800

    终于写出来kmp了,本科时间死磕2次都放弃了.说明严蔚敏那个书这一点讲得不好.是通过看这个网页上的内容实现的http://kb.cnblogs.com/page/176818/

--- C/ch4/src/KMPList.c
+++ C/ch4/src/KMPList.c
@@ -27,17 +27,31 @@ void loadListItem(int** kmpList, char pattern[]) {
 }
 
 int match(char src[], char pattern[],
-        int srcIndex, int patternIndex) {
-    int i = patternIndex;
-    for (; pattern[i] != '\0' &&
-        src[index + i] == pattern[i]; i++) {
-            ;
+        int* srcIndex, int* patternIndex, int* kmpList) {
+    for (; pattern[*patternIndex] != '\0' &&
+        src[*srcIndex] == pattern[*patternIndex];
+        (*srcIndex)++, (*patternIndex)++) {
+            printf("for patternIndex=%d\n", *patternIndex);
+            printf("for srcIndex=%d\n", *srcIndex);
     }
-    return pattern[i] == '\0' ? -1 : i;
+    if (pattern[*patternIndex] == '\0') {
+        return -1;
+    } else {
+        if (*patternIndex == 0) {
+            (*srcIndex)++;
+        } else {
+            *patternIndex =
+                *(kmpList + *patternIndex - 1);
+        }
+        printf("out patternIndex=%d\n", *patternIndex);
+        printf("out srcIndex=%d\n", *srcIndex);
+        return 0;
+    }
 }
+
 void search(char src[], char pattern[]) {
     int patternLen = strlen(pattern);
-    int* kmpList = (int*) malloc(patternLen);
+    int* kmpList = (int*) malloc(patternLen * sizeof(int));
     loadListItem(&kmpList, pattern);
     printf("kmplist=");
     for (int i = 0; i < patternLen; i++) {
@@ -46,21 +60,25 @@ void search(char src[], char pattern[]) {
     printf("\n");
 
     int result = -1;
-    for (int i = 0; src[i] != '\0';) {
-        int unMatchPos = match(src, pattern, i);
+    for (int srcIndex = 0, patternIndex = 0; src[srcIndex] != '\0';) {
+        printf("before loop srcIndex=%d\n",srcIndex);
+        int unMatchPos = match(src, pattern, &srcIndex, &patternIndex,
+                kmpList);
         if (unMatchPos == -1) {
-            result = i;
+            result = srcIndex;
             break;
         }
-        i = (unMatchPos == 0) ? i + 1 :
-            (i + unMatchPos - *(kmpList + unMatchPos));
-        printf("i=%d\n", i);
+        printf("unMatchPos=%d", unMatchPos);
+        // i = (unMatchPos == 0) ? i + 1 :
+        //     (i + unMatchPos - *(kmpList + unMatchPos));
+        // printf("i=%d\n", i);
     }
-    printf("first match pos is :%d", result);
+    printf("first match pos is :%d", result - patternLen);
+    free(kmpList);
 }
 
 void main() {
-    char src[] =     "bbc abcdab abcdabcdabde";
-    char pattern[] =                "abcdabd";
+    char src[] = "bbc abcdab abcdabcdabde";
+    char pattern[] = "abcdabd";
     search(src, pattern);
 }
