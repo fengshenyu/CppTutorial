f85005fb808f95eaa7b49db8bcbe06c879ac4e21
zhouyang209117 <zhouyang209117@sina.com>
2016-04-25 21:25:50 +0800

    简化逻辑.

--- C/DS/Prepare/Structure.c
+++ C/DS/Prepare/Structure.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <string.h>
 typedef struct {
 	int stuNum;
 	char* stuName;
 }Student;
 
-void initCharSpace(char* address, int number) {
-	for (int i = 0; i < number; i++) {
-		*(address + i) = '\0';
-	}
-}
-
 void main() {
 	Student stu1;
 	stu1.stuNum = 1;
-	char* tmpStuNum = (char *)malloc(6);
-	initCharSpace(tmpStuNum, 6);
-	tmpStuNum = "name1";
-	stu1.stuName = tmpStuNum;
+	stu1.stuName = "name1";
 	printf("stuNum=%d,stuName=%s\n", stu1.stuNum, stu1.stuName);
 
 	Student stu2;
 	stu2.stuNum = 2;
-	tmpStuNum = (char *)malloc(6);
-	initCharSpace(tmpStuNum, 6);
-	tmpStuNum = "name2";
-	stu2.stuName = tmpStuNum;
+	stu2.stuName = "name2";
 	printf("stuNum=%d,stuName=%s\n", stu2.stuNum, stu2.stuName);
 }
--- C/DS/Prepare/StructurePointer.c
+++ C/DS/Prepare/StructurePointer.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
-#include <malloc.h>
 typedef struct StudentStruct{
 	int stuNum;
 	char* stuName;
 	struct StudentStruct* next;
 }Student;
 
-void initCharSpace(char* address, int number) {
-	for (int i = 0; i < number; i++) {
-		*(address + i) = '\0';
-	}
-}
-
 void main() {
 	Student stu1;
 	stu1.stuNum = 1;
-	char* tmpStuNum = (char *)malloc(6);
-	initCharSpace(tmpStuNum, 6);
-	tmpStuNum = "name1";
-	stu1.stuName = tmpStuNum;
+	stu1.stuName = "name1";
 
 	Student stu2;
 	stu2.stuNum = 2;
-	tmpStuNum = (char *)malloc(6);
-	initCharSpace(tmpStuNum, 6);
-	tmpStuNum = "name2";
-	stu2.stuName = tmpStuNum;
+	stu2.stuName = "name2";
 
 	stu1.next = &stu2;
 	stu2.next = NULL;
