93a9df3776d2b6487c8a97d0f6b39edd26e2e5d3
zhouyang209117 <zhouyang209117@sina.com>
2016-04-29 20:26:55 +0800

    静态链表.

---
+++ C/DS/List/StaticLink.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define true 1
+#define false 0
+typedef int bool;
+
+#define TOTAL_SIZE 5000
+typedef struct StudentStruct{
+    int stuNum;
+    char* stuName;
+}Student;
+typedef struct {
+    Student data;
+    int next;
+}Node, StaticLink[TOTAL_SIZE];
+
+void init(StaticLink staticLink) {
+    staticLink[0].next = 0;
+}
+void printData(Student data) {
+    pritf("num=%d,name=%s", data.stuNum, data.stuName);
+}
+
+bool add
+bool add(StaticLink staticLink, int index, Student stu) {
+    if (index < 0) {
+        return false;
+    }
+    return true;
+}
+void main() {
+    StaticLink staticLink;
+    init(staticLink);
+    printf("%d\n", staticLink[0].next);
+}
