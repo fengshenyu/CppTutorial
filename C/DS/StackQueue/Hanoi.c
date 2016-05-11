b94a26ae4a89002a4b9183544fcfa977576aaf63
zhouyang3 <zhouyang3@kingsoft.com>
2016-05-11 13:12:32 +0800

    汉诺塔实现.

--- C/DS/StackQueue/Hanoi.c
+++ C/DS/StackQueue/Hanoi.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-void hanoi(int n) {
+int count = 1;
+void move(int diskNum, char a, char b) {
+    printf("%d.Move the %d disk from %c to %c\n", count++, diskNum, a, b);
+}
+void hanoi(int n, char x, char y, char z) {
     if (n == 1) {
-        printf("move from X to Z");
+        move(n, x, z);
     } else {
-        
+        hanoi(n - 1, x, z, y);
+        move(n, x, z);
+        hanoi(n - 1, y, x, z);
     }
 }
+
 int main() {
+    hanoi(3, 'X', 'Y', 'Z');
     return 0;
 }
